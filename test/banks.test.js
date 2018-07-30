47d9da1a84f0b7c1b5b18bc7d719c60fa0bfd686
ashinzekene <ashinzekene@gmail.com>
2018-07-30 22:11:44 +0100

    integrated tests

--- test/banks.test.js
+++ test/banks.test.js
@@ -3,14 +3,26 @@ const supertest = require('supertest');
 const request = supertest(require('../server.js'));
 const expect = chai.expect;
 
-describe('POST /banks', () => {
+describe('GET /banks', () => {
   it('Should should show banks', done => {
-    request.post('/api/banks')
+    request.get('/api/banks')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .end(resp => {
+      .end((err, resp) => {
+        if (err) done(err)
         expect(resp).to.not.be.undefined;
-        expect(resp.body).to.haveOwnProperty('data');
+        expect(resp.body).to.an('array')
+        done();
+      })
+  })
+  it('Should should show banks with bankname bankcode and internetbanking', done => {
+    request.get('/api/banks')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .end((err, resp) => {
+        if (err) done(err)
+        expect(resp).to.not.be.undefined;
+        expect(resp.body[0]).to.have.keys('bankname', 'bankcode', 'internetbanking');
         done();
       })
   })

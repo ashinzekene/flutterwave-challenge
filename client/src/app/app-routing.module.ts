77037aedd4d83e8966fa7d6abff63537c0e32d73
ashinzekene <ashinzekene@gmail.com>
2018-07-31 19:19:58 +0100

    changed to sidebar

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'employees', loadChildren: './employees/employees.module#EmployeesPageModule' },
+  { path: 'products', loadChildren: './products/products.module#ProductsPageModule' },
+  { path: 'payments', loadChildren: './payments/payments.module#PaymentsPageModule' },
   { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
-  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' }
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

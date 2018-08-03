c80ac848d0ed3b7c6611c7686498fa8757bc777c
ashinzekene <ashinzekene@gmail.com>
2018-08-03 04:47:41 +0100

    rebuild

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'staff-mgmt.herokuapp.com/api',
+  base_url: '//staff-mgmt.herokuapp.com/api',
   public_key: 'FLWPUBK-f836a1f887a847ea500cbffcfee9ce45-X'
 };
